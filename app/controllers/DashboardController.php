ba54c013b4b04365df5117f8d0d39301ed4b826d
istifano <elamiriamir7@gmail.com>
2025-02-10 00:12:07 +0100

    j ai crier la base de donner avec l ajoute fake data

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -6,25 +6,25 @@ use App\Auth\AuthController;
 
 class DashboardController{
 
- private AuthController $auth;
-public function __construct()
-{
-    $this->auth = new AuthController;
-}
-public function admin(){
-    if ($this->auth->isLogin()) {
-        header('Location: /DashboardAdmin');
-        exit();
-    }
-    require_once dirname(__DIR__, 1) . '\\views\\pages\\dashboardadmin.php';
-}
-public function client(){
+//  private AuthController $auth;
+// public function __construct()
+// {
+//     $this->auth = new AuthController;
+// }
+// public function admin(){
+//     if ($this->auth->isLogin()) {
+//         header('Location: /DashboardAdmin');
+//         exit();
+//     }
+//     require_once dirname(__DIR__, 1) . '\\views\\pages\\dashboardadmin.php';
+// }
+// public function client(){
 
-}
+// }
 
-public function freelencer(){
+// public function freelencer(){
 
-}
+// }
 
 
 
