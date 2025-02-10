70c101052947a2ed6c1a726f9fbb5a0db209b8f8
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-02-10 04:49:58 +0100

    J'ai Crée la fonctionnalities de l'ajoute d'un offre et de affichier tout les services existes

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -9,6 +9,7 @@ use app\Controllers\MainController;
 
     class ProjectController extends MainController{
         private Project $project;
+        private $id_supprimer;
         public function __construct()
         {
            $this->project = new Project();
@@ -19,15 +20,49 @@ use app\Controllers\MainController;
         }
         
 
+        
+
         public  function findprojects(){
-            $khara = $this->project->selectAll('Projets');
-            var_dump($khara);
+            $tys = $this->project->selectAll('Projets');
+            foreach ($tys as $tyys){
+                echo "<div class='bg-white p-4 rounded shadow'>
+                <h3 class='text-lg font-bold'>$tyys->titre</h3>
+                <p>$tyys->description</p>
+                <p class='text-sm text-gray-600'>Budget: $tyys->budget</p>
+                <p class='text-sm text-gray-600'>Durée: $tyys->duration</p>
+                <div class='flex justify-end space-x-2 mt-2'>
+                    <button class='bg-yellow-500 text-white px-3 py-1 rounded-md hover:bg-yellow-600'>Modifier</button>
+                    <form method='post'>
+                         <input type='submit' name='supprimmer' value='Supprimmer' class='bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600'>
+                    </form>
+                    
+                </div>
+           </div>";
+        //    if(isset($_POST['supprimmer'])){
+        //     $this->project->delete('Projets', $tyys->id);
+            
+        //    }
+            }
         }
 
         public  function addprojects(){
-            // $data = ['titre' => 'hazdhyhy'];
-            // $this->project->insert('Projets', $data);
+            $titre = $_POST['titre'];
+            $description = $_POST['description'];
+            $budget = $_POST['budget'];
+            $duration = $_POST['duration'];
+            $photo = $_POST['photo'];
+
+            $data = ['titre' => "$titre", 'description' => "$description", 'budget' => $budget, 'duration' => $duration, 'photo' => "$photo"];
+            $this->project->insert('Projets', $data);  
+            header('location: ../project');          
         }
-        
+
+    
+        public  function deleteprojects(){
+            
+          $this->project->delete('Projets', 1);
+              
+                     
+        }
     }
 ?>
